243d51ef55d99498b9cd4795abccc9baa16c3b91
Mark Tanner <quakeholio@gmail.com>
2016-02-13 08:43:30 -0700

    getting further along but not done

--- assignment2/madlibs.js
+++ assignment2/madlibs.js
@@ -1,10 +1,6 @@
 /**
  * Created by Mark on 2/11/2016.
- */
-
-var madlib = document.getElementById('div_2');
-//madlib.style.display = 'none';  // hides the elements
-var text_1, text_2, text_3, text_4, text_5, text_6, text_7, text_8, text_9, text_10, text_11, text_12; // Declares all the variables
+*/
 text_1 = document.getElementById('text_1').value;
 text_2 = document.getElementById('text_2').value;
 text_3 = document.getElementById('text_3').value;
@@ -18,65 +14,23 @@ text_10 = document.getElementById('text_10').value;
 text_11 = document.getElementById('text_11').value;
 text_12 = document.getElementById('text_12').value;
 
-var madlib = document.getElementById('div_2');
-madlib.style.visibility = 'hidden';
-madlib.style.visibility = 'visible';
 
-var one = document.getElementById('place_one');
-var two = document.getElementById('place_two');
-var three = document.getElementById('place_three');
-var four = document.getElementById('place_four');
-var five = document.getElementById('place_five');
-var six = document.getElementById('place_six1');/**
- * Created by Mark on 2/11/2016.
- */
+var finishedMadlib = 'It was during the battle of ' + text_1 + ' ' +
+'when I was running through a ' + text_2 + ' ' +
+'when a ' + text_3 + ' ' +
+'went off right next to my platoon. Our ' + text_4 + ' ' +
+'yelled for us to ' + text_5 + ' ' +
+'to the nearest ' + text_6 + ' ' +
+'we could find. When we got to the ' + text_6 + ' ' +
+'we ' + text_7 + ' ' +
+'to start a fire. As we were starting the fire the enemy saw the ' + text_8 + ' ' +
+'from the fire and started ' + text_9 + ' ' +
+' ' + text_10 + ' ' +
+'at us. we all quickly ducked behind the ' + text_11 + ' ' +
+'at the ' + text_6 + ' ' +
+'and returned fire. we quickly eliminated the enemy and were ' + text_12 + ' ' +
+'that we had won the battle.';
 
-var madlib = document.getElementById('div_2');
-//madlib.style.display = 'none';  // hides the elements
-var text_1, text_2, text_3, text_4, text_5, text_6, text_7, text_8, text_9, text_10, text_11, text_12; // Declares all the variables
-text_1 = document.getElementById('text_1').value;
-text_2 = document.getElementById('text_2').value;
-text_3 = document.getElementById('text_3').value;
-text_4 = document.getElementById('text_4').value;
-text_5 = document.getElementById('text_5').value;
-text_6 = document.getElementById('text_6').value;
-text_7 = document.getElementById('text_7').value;
-text_8 = document.getElementById('text_8').value;
-text_9 = document.getElementById('text_9').value;
-text_10 = document.getElementById('text_10').value;
-text_11 = document.getElementById('text_11').value;
-text_12 = document.getElementById('text_12').value;
+var d2 = document.getElementById('div_2');
+d2.innerHTML = finishedMadlib;
 
-var madlib = document.getElementById('div_2');
-madlib.style.visibility = 'hidden';
-madlib.style.visibility = 'visible';
-
-var one = document.getElementById('place_one');
-var two = document.getElementById('place_two');
-var three = document.getElementById('place_three');
-var four = document.getElementById('place_four');
-var five = document.getElementById('place_five');
-var six1 = document.getElementById('place_six1');
-var six2 = document.getElementById('place_six2');
-var six3 = document.getElementById('place_six3');
-var seven = document.getElementById('place_seven');
-var eight = document.getElementById('place_eight');
-var nine = document.getElementById('place_nine');
-var ten = document.getElementById('place_ten');
-var eleven = document.getElementById('place_eleven');
-var twelve = document.getElementById('place_twelve');
-
-one.innerHTML = text_1;
-two.innerHTML = text_2;
-three.innerHTML = text_3;
-four.innerHTML = text_4;
-five.innerHTML = text_5;
-six1.innerHTML = text_6;
-six2.innerHTML = text_6;
-six3.innerHTML = text_6;
-seven.innerHTML = text_7;
-eight.innerHTML = text_8;
-nine.innerHTML = text_9;
-ten.innerHTML = text_10;
-eleven.innerHTML = text_11;
-twelve.innerHTML = text_12;
