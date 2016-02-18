e707567537bec5996e71bad5c80d72b6e30e13e0
Mark Tanner <markchristanner@gmail.com>
2016-02-17 21:48:27 -0700

    need to get a few changes pushed up

---
+++ assignment3/jsmadlibs.js
@@ -0,0 +1,29 @@
+/*
+var getmad = function() {
+    console.log("Hello there old chap"); // just to make sure stuff's happenin'
+    var responses = document.getElementsByTagName('input');
+    for (var i = 0; i < responses.length; i++) {
+    if (responses[i].type === "text") {
+       var iname = "item_" + i;
+       console.log(iname);
+       var m = document.getElementById(iname);
+       var nt = document.createTextNode(responses[i].value);
+       m.appendChild(nt);
+    }
+}
+
+document.getElementById('madlib').setAttribute('class', 'visible'); };
+    var sb = document.getElementById('submit');
+    sb.addEventListener('click', function(ob) {
+    ob.preventDefault();
+    getmad()
+}, false);
+*/
+
+$(document).master();
+
+var master() {
+$(#submit).on('submit', function () {
+        
+    })
+}
