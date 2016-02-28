41b31e87303126b5d8d7b19c3120ef217f548c6e
Mark C Tanner <markchristanner@gmail.com>
2016-02-27 18:08:04 -0700

    moving to work on other computer

--- assignment4/js_with_beer.js
+++ assignment4/js_with_beer.js
@@ -1,3 +1,15 @@
 /**
  * Created by marktanner on 2/27/16.
  */
+$(allTheThings)
+
+function allTheThings() {
+
+
+}
+
+$('#menu').hover(
+    function () { $(this).addClass('hidden'); },
+    function () { $(this).removeClass('hidden'); }
+);
+
