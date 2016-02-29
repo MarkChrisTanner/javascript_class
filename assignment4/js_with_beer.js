9976626c31ad6dcb545a387d330dbf62537d66cf
Mark Tanner <quakeholio@gmail.com>
2016-02-28 20:06:50 -0700

    getting most of the way to the end

--- assignment4/js_with_beer.js
+++ assignment4/js_with_beer.js
@@ -1,15 +1,50 @@
 /**
  * Created by marktanner on 2/27/16.
  */
-$(allTheThings)
+/* http://www.tutorialspoint.com/jqueryui/jqueryui_menu.htm
+got some of the code and notes from this site
+ */
+var menuV=false
+
+function menuHide() {
+    $('#menuBox').fadeOut();
+}
+
+function menuShow() {
+    $('#menuBox').fadeIn();
+}
 
 function allTheThings() {
+    /*
+    $('#menu1').hover(
+        function () { $(this).addClass('hidden'); },
+        function () { $(this).delay(1000).removeClass('hidden'); }
+    );
+    */
+    $('#menu1').menu();
+    $('header').text('Not the Beer you were looking for page you were looking for')
 
+    $('#button').on('click', function(e) {
+        e.preventDefault();
+        if (menuV == false) {
+            menuShow();
+            menuV=true;
+        } else {
+            menuV=false;
+            menuHide();
+        }
+
+    });
+
+    $(".right").on({
+        mouseenter: function () {
+            $('.right').text('something something darkside');
+        },
+        mouseleave: function () {
+            $('.right').text('This is the end of something');
+        }
+    });
 
 }
 
-$('#menu').hover(
-    function () { $(this).addClass('hidden'); },
-    function () { $(this).removeClass('hidden'); }
-);
-
+$(allTheThings)
